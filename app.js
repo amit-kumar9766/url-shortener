519b2a54abebefcc2a9a9165beeb463cf7664a60
Amit Kumar Balhara <amitbalharakr93@gmail.com>
2025-07-20 23:42:03 +0530

    added health check

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const app = express();
 const urlRoutes = require("./src/routes/urlRoutes");
 const { sequelize } = require("./src/models");
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Health check route
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/", urlRoutes);
 
 sequelize.sync({ alter: true }).then(() => {
   console.log("Database synced");
   app.listen(3000, () =>
-    console.log(`✅ Server running at http://localhost:3000`)
+    console.log(`Server running at http://localhost:3000`)
   );
 });
 
